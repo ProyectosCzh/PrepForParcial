d62a70d46a6bd1a91136a851c767583cad392197
Carlos Zambrana <a2024112447@estudiantes.upsa.edu.bo>
2025-09-14 17:05:59 -0400

    first commit

---
+++ ProductivityTrackerAPI/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace ProductivityTrackerAPI;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
